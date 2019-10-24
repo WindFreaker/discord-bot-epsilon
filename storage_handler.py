155817984d83f873b67c697f0ae92c97a5bb3cec
WindFreaker <windfreaker@gmail.com>
2019-10-24 14:07:15 -0400

    Re-fixed directory dependency
    This should solve any crashes found in testing environments where the files/folders already exist

--- storage_handler.py
+++ storage_handler.py
@@ -2,11 +2,14 @@ import os
 import dropbox
 
 dbx = dropbox.Dropbox(os.getenv('DROPBOX_ACCESS_TOKEN'))
-os.mkdir('extensions')
 
 
 def download_extension(name):
-    dbx.files_download_to_file('extensions/' + name + '.py', '/' + name + '.py')
+    try:
+        dbx.files_download_to_file('extensions/' + name + '.py', '/' + name + '.py')
+    except FileNotFoundError:
+        os.mkdir('extensions')
+        download_extension(name)
 
 
 def read_config(name):
